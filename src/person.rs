547be74540e20be09c516f4eae4e70edcfdfc557
Abigaila Ekierte <arekiert@gmail.com>
2020-11-08 17:25:49 +0000

    user has a vector of records

--- src/person.rs
+++ src/person.rs
@@ -1,44 +1,40 @@
 mod record;
 use record::Record;
 
+
 #[derive(Debug)]
 struct User {
     username: String,
     email: String,
     active: bool,
+    user_records: Vec<Record>,
 }
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
+impl User {
+    fn build_user(email: String, username: String) -> User {
+        User {
+            email,
+            username,
+            active: true,
+            user_records: Vec::new(),
+        }
     }
+
+    // pub fn add_record(mut self, record: Record) -> User {
+    //     self.user_records.push(record)
+    // }
 }
 
-// struct CollectionOfRecords {
-//     pub current_records: Vec<Record>
-// }
 
-// fn add_record(record: Record) -> Vec<Record> {
-//     current_records.push(record);
-// }
 
-// impl CollectionOfRecords {
-//     fn start_records() -> Vec<Record> {
-//         let mut current_records :Vec<&Record> = Vec::new();
-//         // println!("{:#?}", current_records);
-//     }
-// }
 
 pub fn run() {
-    // let email: String = String::from("arekiert@op.pl");
-    // let username: String = String::from("aekiert");
-    //
-    // let user1 = build_user(email, username);
-    //
-    // println!("user is {:#?}", user1);
+     let email: String = String::from("arekiert@op.pl");
+     let username: String = String::from("aekiert");
+     let user1 = User::build_user(email, username);
 
-    let new_record = Record::create_new("date", "time", "activity");
-    println!("{:#?}", new_record);
+     println!("user is {:#?}", user1);
+
+    // let new_record = Record::create_new("date", "time", "activity");
+    // println!("{:#?}", new_record);
 }
