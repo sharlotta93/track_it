130e516f1812564b017bd351836fc0580d153558
Abigaila Ekierte <arekiert@gmail.com>
2020-11-12 22:18:44 +0000

    user can directly mofidy the date - need a function wrapper next

--- src/person.rs
+++ src/person.rs
@@ -26,7 +26,7 @@ impl User {
     }
 
     fn change_date(&mut self, mut record: Record, new_date: String) {
-            record.modify_date(new_date);
+            record.date = new_date;
     }
 }
 
@@ -38,14 +38,14 @@ pub fn run() {
      println!("user is {:#?}", user1);
 
      let mut record1 = Record::create_new("date", "time", "activity");
-     let record2 = Record::create_new("8/11", "12:00", "running");
+     let mut record2 = Record::create_new("8/11", "12:00", "running");
      record1.modify_date(String::from("21/11"));
      record1.modify_time(String::from("14:00"));
      record1.modify_activity(String::from("painting"));
 
      user1.add_record(record1);
      user1.add_record(record2);
-
-     // user1.change_date(record2, String::from("7/11"));
+     user1.user_records[1].date = String::from("7/11");
+    // user1.change_date( user1.user_records[0], String::from("7/11"));
      println!("user is {:#?}", user1);
 }
